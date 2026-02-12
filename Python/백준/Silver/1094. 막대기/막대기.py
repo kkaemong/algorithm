284a8723056c1fc7cca162aca865fcbb4550a473
진준영 <junemay31@naver.com>
2026-02-12 10:00:30 +0900

    [Silver V] Title: 막대기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1094. 막대기/막대기.py
+++ Python/백준/Silver/1094. 막대기/막대기.py
@@ -1,9 +1,20 @@
-x = int(input())
-count = 0
+X = int(input())
+sticks = [64]  # 처음엔 64cm 막대 하나만 있음
 
-while x > 0:
-    if x % 2 == 1:
-        count += 1
-    x //= 2
+while sum(sticks) > X:
+    # 1. 가장 짧은 것을 꺼내서 반으로 자른다
+    sticks.sort()  # 가장 짧은 걸 찾기 위해 정렬
+    shortest = sticks.pop(0)
+    half = shortest // 2
 
-print(count)
+    # 2. 자른 것 중 하나(half)와 나머지의 합을 비교한다
+    # sum(sticks)는 이미 shortest가 빠진 상태이므로 남은 것들의 합임
+    if sum(sticks) + half >= X:
+        # 합이 X보다 크거나 같으면, 나머지 반쪽은 버리고 하나만 다시 넣음
+        sticks.append(half)
+    else:
+        # 합이 X보다 작으면, 자른 두 개를 모두 다시 넣음
+        sticks.append(half)
+        sticks.append(half)
+
+print(len(sticks))  # 남은 막대 개수 출력
