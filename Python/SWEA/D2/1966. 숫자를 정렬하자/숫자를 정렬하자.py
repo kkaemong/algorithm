5c0481cdfe6274e296e2915291b3479033dc6cd7
진준영 <junemay31@naver.com>
2026-06-16 08:32:29 +0900

    [D2] Title: 숫자를 정렬하자, Time: 71 ms, Memory: 59,392 KB -BaekjoonHub

--- Python/SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
+++ Python/SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
@@ -1,13 +1,11 @@
 T = int(input())
-for tc in range(1, T + 1):
+for tc in range(1,T+1):
     N = int(input())
     arr = list(map(int,input().split()))
 
     for i in range(N):
         for j in range(i+1,N):
-            if arr[i] <= arr[j]:
-                continue
-            else:
-                arr[i], arr[j] = arr[j],arr[i]
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
 
     print(f'#{tc}', *arr)
