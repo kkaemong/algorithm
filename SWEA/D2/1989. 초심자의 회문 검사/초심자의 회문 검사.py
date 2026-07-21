d49f57a4f4e7e79fdeac76daeb0ce202969dc9c7
진준영 <junemay31@naver.com>
2026-07-21 11:36:10 +0900

    Update 초심자의 회문 검사.py

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -1,8 +1,25 @@
 T = int(input())
 for tc in range(1,T+1):
    arr = list(input())
+   
+   # 풀이방법 1
    brr = list(reversed(arr))
    if brr == arr:
+       print(f'#{tc} {1}')
+   else:
+       print(f'#{tc} {0}')
+      
+ # 풀이방법 2
+   brr = arr[:]
+   brr.reverse()
+
+   if brr == arr:
+       print(f'#{tc} {1}')
+   else:
+       print(f'#{tc} {0}')
+      
+# 풀이방법 3
+   if arr == arr[::-1]:
        print(f'#{tc} {1}')
    else:
        print(f'#{tc} {0}')
