2a1bf2610a7b6f916ea073f90394c95f4a8260b1
진준영 <junemay31@naver.com>
2026-07-29 10:30:10 +0900

    [D4] Title: 가장 빠른 문자열 타이핑, Time: 87 ms, Memory: 59,136 KB -BaekjoonHub

--- SWEA/D4/3143. 가장 빠른 문자열 타이핑/가장 빠른 문자열 타이핑.py
+++ SWEA/D4/3143. 가장 빠른 문자열 타이핑/가장 빠른 문자열 타이핑.py
@@ -1,7 +1,18 @@
 T = int(input())
 for tc in range(1, T + 1):
-    a,b = input().split()
+    a, b = input().split()
 
-    d = list(a.replace(b,'c'))
+    cnt = 0
+    ans = 0
+    a_len = len(a)
+    b_len = len(b)
 
-    print(f'#{tc} {len(d)}')
+    while a_len > cnt:
+        if a[cnt:cnt+b_len] == b:
+            cnt += b_len
+            ans += 1
+        else:
+            cnt += 1
+            ans += 1
+
+    print(f'#{tc} {ans}')
