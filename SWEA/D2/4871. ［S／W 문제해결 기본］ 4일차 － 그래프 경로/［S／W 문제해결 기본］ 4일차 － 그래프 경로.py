cd0313fa12667532d1b7220d5d9093aeb6920522
진준영 <junemay31@naver.com>
2026-08-11 11:39:08 +0900

    [D2] Title: [S/W 문제해결 기본] 4일차 - 그래프 경로, Time: 64 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D2/4871. ［S／W 문제해결 기본］ 4일차 － 그래프 경로/［S／W 문제해결 기본］ 4일차 － 그래프 경로.py
+++ SWEA/D2/4871. ［S／W 문제해결 기본］ 4일차 － 그래프 경로/［S／W 문제해결 기본］ 4일차 － 그래프 경로.py
@@ -1,33 +1,29 @@
 T = int(input())
 for tc in range(1, T + 1):
-    V, E = map(int,input().split())
+    V, E = map(int, input().split())
 
     graph = [[] for _ in range(V + 1)]
     for _ in range(E):
         a, b = map(int, input().split())
         graph[a].append(b)
 
-    S,G = map(int,input().split())
+    S, G = map(int, input().split())
 
-    visited = [False] * (V+1)
+    visited = [False] * (V + 1)
     stack = [S]
-    ans = []
+    visited[S] = True
     flag = False
 
     while stack:
         now = stack.pop()
+
         if now == G:
             flag = True
             break
-        if visited[now]:
-            continue
-        visited[now] = True
-        ans.append(now)
 
         for next_v in graph[now]:
             if not visited[next_v]:
+                visited[next_v] = True
                 stack.append(next_v)
-    if flag:
-        print(f'#{tc} {1}')
-    else:
-        print(f'#{tc} {0}')
+
+    print(f'#{tc} {1 if flag else 0}')
